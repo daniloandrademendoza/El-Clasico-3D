26d3d56a4c6a42e9ce112689053431c3258b91cb
Danilo Andrade Mendoza <daniloandradem10@email.arizona.edu>
2018-10-16 12:14:16 -0700

    Finished the FCBarcelonaChangePlayerScript.

---
+++ El Clasico 3D Project/Assets/Scripts/FCBarcelonaChangePlayerScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class FCBarcelonaChangePlayerScript : MonoBehaviour {
+    private GameObject player;
+    private int selectedPlayer;
+    private string[] playerNames = { "Messi", "Suarez", "Coutinho", "Rakitic", "Vidal", "Sergio", "Roberto", "Pique", "Umtiti", "Alba", "TerStegen" };
+    private int i;
+    private int j;
+    
+    // Use this for initialization
+    void Start () {
+        selectedPlayer = 0;
+        i = 1;
+        j = 0;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(Input.GetKeyDown(KeyCode.X))
+        {
+           // Debug.Log("Entered");
+            if(selectedPlayer<=11)
+            {
+                selectedPlayer++;
+            }
+            else
+            {
+                selectedPlayer = 1;
+            }
+            for(i=1; i<=11;i++)
+            { 
+                    for(j=0; j<11;j++)
+                    {
+                        if (j + 1 == selectedPlayer)
+                        {
+                        //Debug.Log("Entered2");
+                        Debug.Log(playerNames[j]);
+                        player = GameObject.Find(playerNames[j]);
+                        player.GetComponent<FCBarcelonaPlayerScript>().enabled = true;
+                        
+                        }
+                        else
+                        {
+                        // Debug.Log("Entered3");
+                        //Debug.Log(playerNames[j]);
+                        player = GameObject.Find(playerNames[j]);
+                        player.GetComponent<FCBarcelonaPlayerScript>().enabled = false;
+                        
+                    }
+                    }
+
+
+                
+
+            }
+        }
+	}
+}
